13e42b7d0958d745aed9a2a4ec01d4eb001a28ff
Josna Titus <56665824+JosnaTitus@users.noreply.github.com>
2020-12-02 19:51:05 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,18 +1,16 @@
-#I added lines 10 and 11 to make sure the program returns the definition of acronyms 
-# (e.g., USA or NATO.)
-
 import json
-from difflib import get_close_matches
+from difflib import get_close_matches                  #use difflib to check words similarity
+                                                       #use get_close_matches is a method of difflib to check all posibilities of similar words 
 data = json.load(open("data.json"))
 def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
-    elif w.title() in data:
+    elif w.title() in data:                             #if user entered "UPPER" or "upper" this will check for "Upper" as well.
         return data[w.title()]
-    elif w.upper() in data: #in case user enters words like USA or NATO
+    elif w.upper() in data:                             #in case user enters words like USA or INDIA
         return data[w.upper()]
-    elif len(get_close_matches(w, data.keys())) > 0:
+    elif len(get_close_matches(w, data.keys())) > 0:    #checks the length of the 2 words in dict and the user, to check if the word is similar
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
             return data[get_close_matches(w, data.keys())[0]]
@@ -25,7 +23,7 @@ def translate(w):
 
 word = input("Enter word: ")
 output = translate(word)
-if type(output) == list:
+if type(output) == list:                            #this helps to convert the list to sentence from dict
     for item in output:
         print(item)
 else:
