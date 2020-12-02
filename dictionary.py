f689fd8214aefefca2d831dc5ff30aa650b9419c
Josna Titus <56665824+JosnaTitus@users.noreply.github.com>
2020-12-02 19:41:44 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,33 @@
+#I added lines 10 and 11 to make sure the program returns the definition of acronyms 
+# (e.g., USA or NATO.)
+
+import json
+from difflib import get_close_matches
+data = json.load(open("data.json"))
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data: #in case user enters words like USA or NATO
+        return data[w.upper()]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
+
+word = input("Enter word: ")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
+   
